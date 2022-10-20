764816743a7403e5bdc4c5a88b2a3dad63444aac
AditiKulkarni1697 <aditisujlegaonkar@gmail.com>
2022-10-20 22:53:31 +0530

    second file

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+let name = "naman";
+let rev = ""
+for(i=0;i<=name.length-1;i++){
+    rev = name[i] + rev;
+}
+console.log(rev);
+
+if(name == rev){
+console.log("yes")}
+else{
+ console.log("yes")
+}
---
+++ prime_number.js
@@ -0,0 +1,12 @@
+let num = 13;
+let factor = 0;
+for(i=1;i<=13;i++){
+if(num%i == 0){
+factor++
+}
+}
+if(factor !== 2){
+console.log("yes')}
+else{
+console.log("no")
+}
